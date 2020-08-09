c548f513a603694fabd0bd4ed2f857679652027a
Zack Guard <zacharyguard@gmail.com>
2020-08-09 23:01:38 +0800

    Fix sha

--- src/sha.nim
+++ src/sha.nim
@@ -1,15 +1,10 @@
 import nimsha2
 
-const BufSize = 256
-
 proc getSHA256*(filename: string): string =
   var h = initSHA[SHA256]()
 
   let file = open(filename)
-  var buf = newString(BufSize)
-  
-  while not file.endOfFile:
-    let bytesRead = file.readChars(buf, 0, BufSize)
-    h.update(buf[0..<bytesRead])
+  let data = file.readAll()
+  h.update(data)
 
   return h.final().toHex()
